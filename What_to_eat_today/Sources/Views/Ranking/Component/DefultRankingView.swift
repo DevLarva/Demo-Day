f0d3a68e7cb0a2f3dd63ab64798b7793998691df
백대홍 <bdh3620@naver.com>
2023-09-07 20:49:04 +0900

    FEAT: 찜 목록 새로고침

---
+++ What_to_eat_today/Sources/Views/Ranking/Component/DefultRankingView.swift
@@ -0,0 +1,20 @@
+//
+//  DefultRankingView.swift
+//  What_to_eat_today
+//
+//  Created by 백대홍 on 2023/09/07.
+//
+
+import SwiftUI
+
+struct DefultRankingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DefultRankingView_Previews: PreviewProvider {
+    static var previews: some View {
+        DefultRankingView()
+    }
+}
